3d09c9cf0982d84514827594f7c67719599e98c1
Duarte Almeida <duartemalmeida@gmail.com>
2024-01-09 21:51:50 +0000

    first draft for card component

--- src/components/card.tsx
+++ src/components/card.tsx
@@ -1,88 +1,72 @@
-import {
-  Card,
-  Image,
-  Text,
-  ActionIcon,
-  Badge,
-  Group,
-  Center,
-  Avatar,
-  useMantineTheme,
-  rem,
-} from '@mantine/core';
-import classes from './ArticleCard.module.css';
-import { Book } from '@/types/book';
+//@TO DO
+//1. add logic for bookmark and save icons onclick events once the user model is created
+//2. toggle the icon without the use of hooks (state, refs) and turning component into a client one
+//3. styling still needs some touches
 
-export function ArticleCard({ ...props }: Book) {
+import Link from 'next/link';
+import Image from 'next/image';
+import { FaRegHeart, FaBookmark } from 'react-icons/fa';
+import { FaRegBookmark } from 'react-icons/fa6';
+import { Book } from '@/types/book';
+import StarRating from './Rating';
+
+export default function BookCard({ ...props }: Book) {
   const linkProps = {
     href: `${props.link}`,
     target: '_blank',
     rel: 'noopener noreferrer',
   };
-  const theme = useMantineTheme();
 
   return (
-    <Card withBorder radius='md' className={classes.card}>
-      <Card.Section>
-        <a {...linkProps}>
-          <Image
-            src={props.image || 'placeho    lder'}
-            alt='book cover'
-            height={180}
-          />
-        </a>
-      </Card.Section>
+    <div className='border rounded-md overflow-hidden bg-white relative p-6'>
+      <a {...linkProps}>
+        <Image
+          src={props.image}
+          alt='book cover'
+          className='w-full h-40 object-cover'
+          width={318}
+          height={180}
+        />
+      </a>
 
-      <Badge
-        className={classes.rating}
-        variant='gradient'
-        gradient={{ from: 'yellow', to: 'red' }}
-      >
-        {props.genre}
-      </Badge>
+      <div className='absolute top-3 right-12'>
+        <span className='px-2 py-1 rounded bg-gradient-to-r from-yellow-300 to-red-400'>
+          {props.genre}
+        </span>
+      </div>
 
-      <Text className={classes.title} fw={500} component='a' {...linkProps}>
-        {props.title}
-      </Text>
-
-      <Text fz='sm' c='dimmed' lineClamp={4}>
+      <h2>
+        <Link {...linkProps} className='block mt-3 mb-1 font-semibold'>
+          {props.title}
+        </Link>
+      </h2>
+      <h3 className='text-sm text-dimmed leading-5 line-clamp-4'>
         {props.summary}
-      </Text>
+      </h3>
 
-      <Group justify='space-between' className={classes.footer}>
-        <Center>
-          <Avatar
-            src='https://raw.githubusercontent.com/mantinedev/mantine/master/.demo/avatars/avatar-1.png'
-            size={24}
-            radius='xl'
-            mr='xs'
-          />
-          <Text fz='sm' inline>
-            {props.rating}
-          </Text>
-        </Center>
+      <div className='mt-4 flex justify-between'>
+        <Link
+          href={`/request/new?book=${encodeURIComponent(props.id)}`}
+          className='bg-blue-500 hover:bg-blue-700 text-white font-bold py-2 px-4 rounded-3xl'
+        >
+          Request
+        </Link>
 
-        <Group gap={8} mr={0}>
-          <ActionIcon className={classes.action}>
-            <IconHeart
-              style={{ width: rem(16), height: rem(16) }}
-              color={theme.colors.red[6]}
+        <div className='flex gap-4'>
+          <div className='bg-slate-200 hover:bg-sky-700 p-2 rounded-md flex items-center cursor-pointer'>
+            <StarRating rating={props.rating} />
+          </div>
+          <div className='bg-slate-200 hover:bg-sky-700 p-2 rounded-md flex items-center cursor-pointer'>
+            <FaRegHeart style={{ width: '1rem', height: '1rem' }} color='red' />
+          </div>
+          <div className='bg-slate-200 hover:bg-sky-700 p-2 rounded-md flex items-center cursor-pointer'>
+            <FaRegBookmark
+              style={{ width: '1rem', height: '1rem' }}
+              color='yellow'
             />
-          </ActionIcon>
-          <ActionIcon className={classes.action}>
-            <IconBookmark
-              style={{ width: rem(16), height: rem(16) }}
-              color={theme.colors.yellow[7]}
-            />
-          </ActionIcon>
-          <ActionIcon className={classes.action}>
-            <IconShare
-              style={{ width: rem(16), height: rem(16) }}
-              color={theme.colors.blue[6]}
-            />
-          </ActionIcon>
-        </Group>
-      </Group>
-    </Card>
+          </div>
+        </div>
+      </div>
+    </div>
   );
 }
