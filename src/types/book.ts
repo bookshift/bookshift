31ec4b616530da6e43147fa5e2a7a1b6ab171acb
Duarte Almeida <duartemalmeida@gmail.com>
2024-01-22 22:07:11 +0000

    fetching books functionality and its types (WIP)

--- src/types/book.ts
+++ src/types/book.ts
@@ -1,9 +1,63 @@
 export type Book = {
-  id: number;
+  id: string;
   title: string;
-  summary: string;
-  image: string;
-  rating: number;
-  link: string;
-  genre: string;
+  author: string[] | undefined;
+  summary: string | undefined;
+  image: string | undefined;
+  link: string | undefined;
+  //genre: tbd
+  //rating - needs util function to grab it from db
+};
+
+type IndustryIdentifier = {
+  type: string;
+  identifier: string;
+};
+
+type ReadingModes = {
+  text: boolean;
+  image: boolean;
+};
+
+type ImageLinks = {
+  smallThumbnail: string;
+  thumbnail: string;
+};
+
+type VolumeInfo = {
+  title: string;
+  subtitle?: string;
+  authors?: string[];
+  publishedDate?: string;
+  industryIdentifiers?: IndustryIdentifier[];
+  readingModes?: ReadingModes;
+  pageCount?: number;
+  printType?: string;
+  categories?: string[];
+  maturityRating?: string;
+  allowAnonLogging?: boolean;
+  contentVersion?: string;
+  panelizationSummary?: {
+    containsEpubBubbles: boolean;
+    containsImageBubbles: boolean;
+  };
+  imageLinks?: ImageLinks;
+  language?: string;
+  previewLink?: string;
+  infoLink?: string;
+  canonicalVolumeLink?: string;
+};
+
+export type BookItem = {
+  kind: string;
+  id: string;
+  etag: string;
+  selfLink: string;
+  volumeInfo: VolumeInfo;
+};
+
+export type BookResponse = {
+  kind: string;
+  totalItems: number;
+  items: BookItem[];
 };
