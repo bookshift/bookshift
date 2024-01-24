c4d76e236dc5eb9257aa4d5bbb59b5c813640b32
AnthonyClarke25 <luda59@hotmail.com>
2024-01-24 02:17:45 +0000

    broken in a different way

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -4,8 +4,9 @@ import { useUser, SignOutButton } from "@clerk/nextjs";
 import createUser from "./pages/api/createUser";
 import { BookUser } from "../types/user";
 import userRegistered from "./pages/api/AlreadyRegistered";
+import { use } from "react";
 
-export default async function Home() {
+export default function Home() {
   const { isLoaded, isSignedIn, user } = useUser();
 
   // In case the user signs out while on the page.
@@ -26,44 +27,36 @@ export default async function Home() {
     );
   }
 
-  const alreadyRegistered = async () => {
-    const resgisteredResult = userRegistered(user.id);
+  //const alreadyRegistered = await userRegistered(user.id);
 
-    if (await resgisteredResult) {
-      const returnedUser = await resgisteredResult.then((res) => res);
+  // console.log(alreadyRegistered);
 
-      return returnedUser;
-    }
-  };
+  // if (!alreadyRegistered) {
+  //   const pushdata: BookUser = {
+  //     firstname: user.firstName ?? "",
+  //     lastname: user.lastName ?? "",
+  //     email: user.emailAddresses[0].emailAddress,
+  //     username: user.username || user.emailAddresses[0].emailAddress,
+  //     clerkid: user.id,
+  //   };
 
-  const registedUser = await alreadyRegistered();
+  //   createUser(pushdata);
+  // }
 
-  if (!registedUser) {
-    const pushdata: BookUser = {
-      firstname: user.firstName ?? "",
-      lastname: user.lastName ?? "",
-      email: user.emailAddresses[0].emailAddress,
-      username: user.username || user.emailAddresses[0].emailAddress,
-      clerkid: user.id,
-    };
+  // if (alreadyRegistered) {
+  //   return (
+  //     <main className="flex min-h-screen flex-col items-center justify-between p-24">
+  //       <h1>Bookshift</h1>
 
-    createUser(pushdata);
-  }
+  //       <h1>User Profile</h1>
+  //       <p>
+  //         Name: {alreadyRegistered?.firstname} {alreadyRegistered?.lastname}
+  //       </p>
+  //       <p>Email: {alreadyRegistered?.email}</p>
 
-  if (await registedUser) {
-    return (
-      <main className="flex min-h-screen flex-col items-center justify-between p-24">
-        <h1>Bookshift</h1>
-
-        <h1>User Profile</h1>
-        <p>
-          Name: {registedUser?.firstname} {registedUser?.lastName}
-        </p>
-        <p>Email: {registedUser?.emailAddress}</p>
-
-        <h1> Sign out </h1>
-        <SignOutButton />
-      </main>
-    );
-  }
+  //       <h1> Sign out </h1>
+  //       <SignOutButton />
+  //     </main>
+  //   );
+  // }
 }
