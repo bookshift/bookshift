140ea8484fea4486334a087e5353eee9b6a12d2e
AnthonyClarke25 <luda59@hotmail.com>
2024-02-14 20:56:26 +0000

    reverting page back

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,16 +1,15 @@
 'use client';
 
-import Navbar from '@/components/Navbar';
-import getUser from '@/data-access/users/get-user';
-import { useUser } from '@clerk/nextjs';
-import { redirect } from 'next/navigation';
+import { useUser } from "@clerk/nextjs";
+import { redirect } from "next/navigation";
 
-export default async function Home() {
-  await getUser();
-  return (
-    <main className='flex min-h-screen flex-col items-center justify-between p-24'>
-      <Navbar />
-      <h1>Bookshift</h1>
-    </main>
-  );
+export default function Home() {
+  const { isSignedIn, user } = useUser();
+
+  if (!isSignedIn || !user) {
+    return redirect("/sign-in");
+  }
+
+  // Redirect to dashboard
+  return redirect("/home");
 }
